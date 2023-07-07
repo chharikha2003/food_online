1d1bf80f40f2f77774ecc0341c3976d7cbfc5254
chharikha2003 <csharikha03@gmail.com>
2023-07-07 15:25:23 +0530

    crud operation for categories

--- vendor/urls.py
+++ vendor/urls.py
@@ -5,7 +5,14 @@ from accounts import views as account_views
 
 urlpatterns=[
     path("",account_views.vendorDashboard),
-    path('profile/',views.vprofile,name='vprofile')
+    path('profile/',views.vprofile,name='vprofile'),
+    path('menu-builder/',views.menu_builder,name='menu_builder'),
+    path('menu-builder/category/<int:pk>/',views.fooditems_by_category,name='fooditems_by_category'),
+    #Category CRUD
+    path("menu-builder/category/add/",views.add_category,name="add_category"),
+    path('menu-builder/category/edit/<int:pk>/',views.edit_category,name='edit_category'),
+    path('menu-builder/category/delete/<int:pk>/',views.delete_category,name="delete_category")
+    
     
 
 
